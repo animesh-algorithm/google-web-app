72fb49410e5bf1eeca4ca8e099ac7f693125673d
Animesh Sharma <animesharma3@gmail.com>
2022-08-30 23:30:01 +0530

    Add authentication and get user data from google

---
+++ pages/auth/signin.js
@@ -0,0 +1,41 @@
+import Header from "../../components/Header";
+import { getProviders, signIn } from "next-auth/react";
+
+const signin = ({ providers }) => {
+  return (
+    <>
+      <Header />
+      <div className="mt-40">
+        {Object.values(providers).map((provider) => (
+          <div className="flex flex-col items-center" key={provider.name}>
+            <img
+              className="w-52 object-cover"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png"
+              alt="google logo"
+            />
+            <p className="text-sm italic my-10 text-center">
+              This website is created for learning purposes
+            </p>
+            <button
+              className="bg-red-400 rounded-lg p-3 text-white hover:bg-red-500"
+              onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+            >
+              Sign in with {provider.name}
+            </button>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export async function getServerSideProps(context) {
+  const providers = await getProviders();
+  return {
+    props: {
+      providers,
+    },
+  };
+}
+
+export default signin;

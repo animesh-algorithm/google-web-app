3d728d5221c66cb8380497cc6601a76fe70c4534
Animesh Sharma <animesharma3@gmail.com>
2022-09-01 22:09:55 +0530

    Create search header component

---
+++ pages/search.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import SearchHeader from "../components/SearchHeader";
+
+const search = () => {
+  return (
+    <div>
+      <Head>
+        <title>Search Page</title>
+      </Head>
+      {/* Search Header */}
+      <SearchHeader />
+      {/* Search Results */}
+    </div>
+  );
+};
+
+export default search;
